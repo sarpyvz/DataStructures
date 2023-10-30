f664f9525966a90914a7950af784349ef4810cec
Sarp Yavuz <147308207+sarpyvz@users.noreply.github.com>
2023-10-30 18:55:35 +0300

    Update Bank.cpp

--- Bank/Bank.cpp
+++ Bank/Bank.cpp
@@ -1,47 +1,106 @@
 #include <iostream>
 #include "bank.h"
 #include <vector>
-#include "account.h"
 using namespace std;
 
-Bank::Bank(){
-    vector< Account > accounts;
-}
 
-string Bank::find_account(string name, int password) {
-    for (int acc = 0; acc < accounts.size() ; acc++){
-        for (int i = 0; i < 2; i++){
-            if ( accounts[acc][i] == name || accounts[acc][i+1] == password ){
-                return accounts[acc];
-            }
+Account Bank::find_account(string name, string password) {
+    Bank amo; // direk vector<Account> diyip de olabilir
+    for (int i = 0; i < amo.accounts.size();i++){
+        if (amo.accounts[i].name == name && amo.accounts[i].password == password ){
+            return amo.accounts[i];
         }
-
     }
 
 
 }
 
 
-
-
-
-
-
-bool Bank::createAccount(string name, int password) {
-    cout << "Enter name and password for account creating : " << endl;
-    cin >> name >> password;
+bool Bank::createAccount() {
+    string name,password;
+    double balance;
+    Bank amo;
+    cout << "Enter name and password and balance for account creating : " << endl;
+    cin >> name >> password >> balance;
+    for ( int i = 0; i < amo.accounts.size();i++){
+        if ( amo.accounts[i].name == name && amo.accounts[i].password == password){
+            return false;
+        }
+    }
+    balance = 0.0;
+    amo.accounts.emplace_back(name,password,balance);
+    return true;
 }
 
-bool Bank::deleteAccount(string name, int password) {}
 
-bool Bank::deposit(string name, int password) {}
+bool Bank::deleteAccount(string name, string password) {
+    Bank amo;
+    for (int i = 0; i < amo.accounts.size();i++){
+        if (amo.accounts[i].name == name && amo.accounts[i].password == password){
+            amo.accounts.erase(amo.accounts.begin() + i);
+            return true;
+        }
+    return false;
+    }
 
-bool Bank::getBalance(string name, int password) {}
 
-void Bank::printDatabase(string name, int password, int balance) {}
-
+}
 
 
-bool Bank::withdraw(string name, int password) {}
+bool Bank::deposit(string name, string password) {
+    Bank amo;
+    double money;
+    cout << "Enter the amount you want to deposit : ";
+    cin >> money;
+    for (int i = 0; i < amo.accounts.size();i++){
+        if (amo.accounts[i].name == name && amo.accounts[i].password == password){
+            amo.accounts[i].balance += money;
+            return true;
+        }
+    }
+    return false;
+
+}
 
 
+double Bank::getBalance(string name, string password) {
+    Bank amo;
+
+    for (int i = 0; i < amo.accounts.size();i++){
+        if (amo.accounts[i].name == name && amo.accounts[i].password == password){
+            return amo.accounts[i].balance;
+        }
+    }
+
+
+}
+
+
+void Bank::printDatabase(string name, string password) {
+    Bank amo;
+    for (int i = 0; i < amo.accounts.size(); i++){
+        cout <<"Username: " <<  amo.accounts[i].name <<" Password: " <<  amo.accounts[i].password <<" Balance :" <<amo.accounts[i].balance<< "\n"<< endl;
+    }
+
+}
+
+
+
+bool Bank::withdraw(string name, string password) {
+    Bank amo;
+    double money;
+    cout << "Enter the amount you want to withdraw: ";
+    cin >> money;
+    for (int i = 0; i < amo.accounts.size();i++){
+        if (amo.accounts[i].name == name && amo.accounts[i].password == password){
+            if(amo.accounts[i].balance < money){
+                return false;
+            }
+            else {
+                amo.accounts[i].balance -= money;
+                return true;
+            }
+        }
+    }
+    return false;
+}
