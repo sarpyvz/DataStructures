fe91c3dc79873e2dfaf2722dc6aa775e21dad574
Sarp Yavuz <147308207+sarpyvz@users.noreply.github.com>
2023-10-30 18:56:48 +0300

    Update main.cpp

--- Bank/main.cpp
+++ Bank/main.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include "Bank.cpp"
+
+using namespace std;
+
+int main()
+{
+    Bank DataBank;
+    DataBank.createAccount();
+    DataBank.createAccount();
+    DataBank.printDatabase("amo","123");
+
+    DataBank.deposit("amo", "123");
+    DataBank.withdraw("amo", "123");
+    DataBank.printDatabase("amo","123");
+
+    return 0;
+}
