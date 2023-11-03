6b7da89ee0c777dc43fcd49fcf40a9123ed722fb
Sarp Yavuz <147308207+sarpyvz@users.noreply.github.com>
2023-11-03 17:29:53 +0300

    Update ServiceRecord.cpp

--- ServiceCenter/ServiceRecord.cpp
+++ ServiceCenter/ServiceRecord.cpp
@@ -3,8 +3,10 @@
 
 void ServiceRecord::addPartChanged(const std::string &part)
 {
+    ServiceRecord::partsChanged.push_back(part);
 }
 
 void ServiceRecord::displayRecord() const
 {
+    std::cout << ServiceRecord::getPartsChanged<< ServiceRecord::getVehicle<< ServiceRecord::getServiceDate << std::endl;
 }
